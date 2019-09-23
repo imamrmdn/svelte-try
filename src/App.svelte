2d9e4b27adc5bd6543a38ca666a41d5c807f3255
imamrmdn <imamramadhan420@gmail.com>
2019-09-23 14:08:04 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+
+</script>
+
+<style>
+  nav {
+    background: #eb3d34;
+  }
+  .container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 2rem;
+    min-height: 4rem;
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  div p {
+    padding: 0 0.5rem;
+    color: #fff;
+    font-size: 1.1rem;
+	font-weight: 500;
+    cursor: pointer;
+  }
+
+  .brand {
+	  font-size: 1.2rem;
+  }
+
+</style>
+
+<nav>
+  <div class="container">
+
+    <p class="brand">Simantek</p>
+
+    <div class="row">
+      <p class="item">Sign in</p>
+      <p class="item">Sign up</p>
+    </div>
+  </div>
+</nav>
